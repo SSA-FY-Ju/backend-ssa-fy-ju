802d2ca57ee0ffa1a33e30391c92df02c060be46
SUNMI <hariaus@naver.com>
2026-04-14 21:10:28 +0900

    Initial commit

---
+++ SSAju/src/main/java/ssafy/SSAju/SsAjuApplication.java
@@ -0,0 +1,13 @@
+package ssafy.SSAju;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SsAjuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SsAjuApplication.class, args);
+	}
+
+}
