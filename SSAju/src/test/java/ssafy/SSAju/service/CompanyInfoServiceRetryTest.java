1d9c37c8ac12b835f2d28e709fea1eeafe7eeb89
SUNMI <hariaus@naver.com>
2026-08-12 23:16:19 +0900

    refactor: CompanyInfoService 테스트 RestClient mock 보일러플레이트 공통 베이스로 추출
    CompanyInfoServiceTest와 CompanyInfoServiceRetryTest가 각자
    RequestHeadersUriSpec/RequestHeadersSpec/ResponseSpec mock 필드와
    동일한 체이닝 스텁 코드를 반복 작성하고 있어 CompanyInfoServiceTestSupport
    추상 클래스로 공통화. RestClient 체이닝 방식이 바뀌면 한 곳만 수정하면
    되도록 정리.
    
    [Test Passed]

--- SSAju/src/test/java/ssafy/SSAju/service/CompanyInfoServiceRetryTest.java
+++ SSAju/src/test/java/ssafy/SSAju/service/CompanyInfoServiceRetryTest.java
@@ -1,23 +1,17 @@
 package ssafy.SSAju.service;
 
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.Mock;
-import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.http.HttpStatus;
-import org.springframework.test.util.ReflectionTestUtils;
 import org.springframework.web.client.HttpClientErrorException;
 import org.springframework.web.client.HttpServerErrorException;
-import org.springframework.web.client.RestClient;
+import ssafy.SSAju.dto.external.PublicDataApiResponse;
 
 import java.time.LocalDate;
 import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
-import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.BDDMockito.given;
 
 /**
@@ -25,33 +19,12 @@ import static org.mockito.BDDMockito.given;
  * 5xx는 원본 예외 그대로 전파되어 {@code @Retryable}이 재시도할 수 있어야 하고,
  * 4xx는 즉시 빈 결과로 처리되어 재시도하지 않아야 한다.
  */
-@ExtendWith(MockitoExtension.class)
 @DisplayName("CompanyInfoService 재시도 정책 단위 테스트")
-class CompanyInfoServiceRetryTest {
+class CompanyInfoServiceRetryTest extends CompanyInfoServiceTestSupport {
 
-    @Mock
-    private RestClient publicDataRestClient;
-
-    @Mock private RestClient.RequestHeadersUriSpec<?> requestHeadersUriSpec;
-    @Mock private RestClient.RequestHeadersSpec<?> requestHeadersSpec;
-    @Mock private RestClient.ResponseSpec responseSpec;
-
-    private CompanyInfoService companyInfoService;
-
-    @BeforeEach
-    void setUp() {
-        companyInfoService = new CompanyInfoService(publicDataRestClient);
-        ReflectionTestUtils.setField(companyInfoService, "apiKey", "test-api-key");
-        ReflectionTestUtils.setField(companyInfoService, "publicDataUrl",
-                "https://apis.data.go.kr/1160100/service/GetCorpBasicInfoService_V2");
-    }
-
-    @SuppressWarnings("unchecked")
     private void givenRestClientThrows(RuntimeException exception) {
-        given(publicDataRestClient.get()).willReturn((RestClient.RequestHeadersUriSpec) requestHeadersUriSpec);
-        given(requestHeadersUriSpec.uri(any(java.net.URI.class))).willReturn((RestClient.RequestHeadersSpec) requestHeadersSpec);
-        given(requestHeadersSpec.retrieve()).willReturn(responseSpec);
-        given(responseSpec.body(ssafy.SSAju.dto.external.PublicDataApiResponse.class)).willThrow(exception);
+        stubRetrieve();
+        given(responseSpec.body(PublicDataApiResponse.class)).willThrow(exception);
     }
 
     @Test
